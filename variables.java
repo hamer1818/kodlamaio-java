cfcce29881a5b5e87a444ca60213c088846b433f
hamer1818 <47240233+hamer1818@users.noreply.github.com>
2022-09-18 18:06:04 +0300

    Create variables.java
    java daki değişkenler

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class Main {
+    //reusability
+    public static void main(String[] args) {
+        String mesaj = "Öğrenci Sayısı : ";
+        int ogrenciSayi = 10;
+        System.out.println(mesaj + ogrenciSayi);
+        System.out.println(mesaj + ogrenciSayi);
+        System.out.println("Öğrenci sayısı : " + ogrenciSayi);
+        System.out.println("Öğrenci sayısı : " + ogrenciSayi);
+    }
+}
